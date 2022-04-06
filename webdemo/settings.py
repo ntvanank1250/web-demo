e5ac70f2d2ead0d2a4c2ff71541f87ef40240dd3
ntvanank1250 <doduyhieu12091998@gmail.com>
2022-04-06 13:57:01 +0700

    nhung css

--- webdemo/settings.py
+++ webdemo/settings.py
@@ -120,9 +120,8 @@ STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
+    BASE_DIR / "static",
 ]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
